6bd27dc7bdc976a5bc5ef359c09bc9ae829e7f27
Kazimierz Lipski <kazimierz.lipski1@gmail.com>
2023-04-14 10:37:12 +0200

    Changed flag

--- c/server/run_container.sh
+++ c/server/run_container.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --network-alias z33_c_server --hostname z33_c_server --network z33_network --name z33_c_server z33_task1_1_c_server --ip 172.21.33.0 8000
+docker run -it --rm --network-alias z33_c_server --hostname z33_c_server --network z33_network --name z33_c_server z33_task1_1_c_server --ip 172.21.33.0 8000
