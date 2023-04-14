7aba90ccc8619314a6443e53accc70fa7f67111f
Kazimierz Lipski <kazimierz.lipski1@gmail.com>
2023-04-14 10:30:21 +0200

    Made console look nice

--- c/server/turn_on_server.sh
+++ c/server/turn_on_server.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 sh ./create_container.sh
+sh "clear"
+echo "
+Container created
+"
+sh "docker images | grep z33"
+sleep 1s
+sh "clear"
+echo "
+Running container...
+"
 sh ./run_container.sh
